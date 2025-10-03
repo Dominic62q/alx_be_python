20b42ddbd5b2936862b9b5d720ac76275553e227
Dominic62q <dominicquainoo62@gmail.com>
2025-10-03 17:00:02 +0000

    Add bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
-    def __init__(self, account_balance):
+    def __init__(self, account_balance=0):  # default balance is 0
         self.account_balance = account_balance
+
 
     def deposit(self, amount):
         self.account_balance += amount
