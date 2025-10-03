71b0d0d7eaf318fbd7c893638d48611a89f478ca
Dominic62q <dominicquainoo62@gmail.com>
2025-10-03 16:56:40 +0000

    Add bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:   # funds are enough
+            self.account_balance -= amount
+            return True                      # success
+        else:
+            return False                     # insufficient funds, no change
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+
